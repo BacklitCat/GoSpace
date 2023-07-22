d4323ecf0a3b96e836ddd33e361ccb370ae45e4d
BacklitCat <BacklitCat@gmail.com>
2023-07-23 00:00:00 +0800

    feat: add PgDAO

---
+++ internal/dao/postgresql_dao.go
@@ -0,0 +1,66 @@
+package dao
+
+import (
+	"GoSpace/internal/client"
+	"GoSpace/internal/errno"
+	"GoSpace/internal/model"
+	"fmt"
+	"github.com/go-pg/pg/v10"
+)
+
+type PgDAO struct {
+}
+
+func (p *PgDAO) CreateUser(user *model.UserBasic) error {
+	clt := client.ClientManager.GetPgClient()
+	_, err := clt.DB.Model(user).Insert(user)
+	if err != nil {
+		switch err.(pg.Error).Field('C') {
+		case "23505":
+			return errno.NewErrorNo(err, errno.ErrSignUpEmailOrPhoneDuplicate)
+		}
+	}
+	return nil
+}
+
+func (p *PgDAO) GetUser(user *model.UserBasic) error {
+	if len(user.Email) > 0 {
+		return p.GetUserByEmail(user)
+	} else if len(user.Phone) > 0 {
+		return p.GetUserByPhone(user)
+	}
+	return errno.NewErrorNo(nil, errno.ErrSelectUserUnknownEmailOrPhone)
+}
+
+func (p *PgDAO) GetUserByEmail(user *model.UserBasic) error {
+	clt := client.ClientManager.GetPgClient()
+	fmt.Println(user.Email)
+	err := clt.DB.Model(user).
+		Where("email = ?", user.Email).
+		Select()
+	if err != nil {
+		switch err.Error() {
+		case "pg: no rows in result set":
+			return errno.NewErrorNo(err, errno.ErrSelectUserByEmailEmpty)
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+func (p *PgDAO) GetUserByPhone(user *model.UserBasic) error {
+	clt := client.ClientManager.GetPgClient()
+	err := clt.DB.Model(user).
+		Where("phone = ?", user.Phone).
+		Select()
+	if err != nil {
+		switch err.Error() {
+		case "pg: no rows in result set":
+			return errno.NewErrorNo(err, errno.ErrSelectUserByPhoneEmpty)
+		default:
+			return err
+		}
+	}
+	return nil
+}
