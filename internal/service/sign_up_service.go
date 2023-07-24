5c30ef3011cf0ac5ed84e9428d7ece81fdaabdf7
BacklitCat <BacklitCat@gmail.com>
2023-07-24 23:58:48 +0800

    const: 更新常量位置，修改部分常量

--- internal/service/sign_up_service.go
+++ internal/service/sign_up_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"GoSpace/internal/common"
 	"GoSpace/internal/dao"
 	"GoSpace/internal/errno"
 	"GoSpace/internal/model"
@@ -12,17 +13,17 @@ type SignUpService struct {
 
 func (s *SignUpService) SignUp(email, phone, pwd string) (id int64, err error) {
 	if len(email) == 0 && len(phone) == 0 {
-		return 0, errno.NewErrorNo(nil, errno.ErrUnknownEmailAndPhone)
+		return 0, errno.NewErrorNo(nil, common.ErrUnknownEmailAndPhone)
 	}
 
 	if len(email) > 0 && !util.IsEmailValid(&email) {
-		return 0, errno.NewErrorNo(nil, errno.ErrNotValidEmail)
+		return 0, errno.NewErrorNo(nil, common.ErrNotValidEmail)
 	}
 	if len(phone) > 0 && !util.IsPhoneValid(&phone) {
-		return 0, errno.NewErrorNo(nil, errno.ErrNotValidPhone)
+		return 0, errno.NewErrorNo(nil, common.ErrNotValidPhone)
 	}
 	if len(pwd) > 0 && !util.IsPasswordValid(&pwd) {
-		return 0, errno.NewErrorNo(nil, errno.ErrNotValidPwd)
+		return 0, errno.NewErrorNo(nil, common.ErrNotValidPwd)
 	}
 
 	user := model.UserBasic{
@@ -35,7 +36,7 @@ func (s *SignUpService) SignUp(email, phone, pwd string) (id int64, err error) {
 	user.Salt = util.RandStringBytesMaskImprSrcUnsafe(8)
 	user.HashPwd, err = util.HashSalt(user.HashPwd, user.Salt)
 	if err != nil {
-		return 0, errno.NewErrorNo(err, errno.ErrUtilHashSalt)
+		return 0, errno.NewErrorNo(err, common.ErrUtilHashSalt)
 	}
 
 	if err = dao.PgDAO.CreateUser(&user); err != nil {
